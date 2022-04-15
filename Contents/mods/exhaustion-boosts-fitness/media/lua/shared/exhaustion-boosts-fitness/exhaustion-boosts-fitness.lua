05838d6e077a1826a266ab3d89689ff6a6ad921c
Rob Lyons <roblyons96@live.com>
2022-04-15 12:28:06 +0100

    initial

---
+++ Contents/mods/exhaustion-boosts-fitness/media/lua/shared/exhaustion-boosts-fitness/exhaustion-boosts-fitness.lua
@@ -0,0 +1,25 @@
+
+
+
+local function playerUpdateMultipliers(player)
+
+    if(player:getStats():getEndurancewarn() ~= 0) then
+        player:getStats():setEndurancewarn(0)
+    end
+    
+    local enduranceLevel = player:getMoodles():getMoodleLevel(MoodleType.Endurance)
+    local painLevel = player:getMoodles():getMoodleLevel(MoodleType.Pain)
+
+    local xpMultiplier = math.floor( ( ( enduranceLevel + 1 ) ^ 3 ) / ( ( painLevel ^ 3 ) + 1 ) )
+    local fitnessPerk = Perks.Fitness
+    local strengthPerk = Perks.Strength
+
+    if xpMultiplier == 1 then
+        xpMultiplier = 0
+    end
+
+	player:getXp():addXpMultiplier(fitnessPerk, xpMultiplier, 0, 10)
+    player:getXp():addXpMultiplier(strengthPerk, xpMultiplier, 0, 10)
+end
+
+Events.OnPlayerUpdate.Add(playerUpdateMultipliers)
---
+++ README.md
@@ -0,0 +1,13 @@
+# zomboid-wood-gassifier
+Adds a Wood Gassifier to provide fuel for generator
+
+## Install
+Coming Soon.
+
+## Requirements / Dependencies
+- None so far
+
+Note: These should be automatically installed on subscribing in Steam Workshop.
+
+## Support
+Just open a github issue or something.
